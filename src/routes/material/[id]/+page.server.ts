53ab32d4a12af37c6e1f9f4c7ce9643da4d9555d
Fred Guth <fredguth@fredguth.com>
2023-11-24 13:26:15 -0300

    create view ItensCompra

--- src/routes/material/[id]/+page.server.ts
+++ src/routes/material/[id]/+page.server.ts
@@ -1,16 +1,34 @@
-import db from "$lib/server/prisma";
-import type { PageServerLoad } from "../$types";
+import db from '$lib/server/prisma'
+import type { PageServerLoad } from '../$types'
 
-let id: string = "";
+let id: string = ''
 export const load = (async ({ params, url }) => {
-   console.log('LOADING ITEMS....')
-		// @ts-ignore
-		const { id } = params
-		const material = await db.material.findFirst({
-			where: {
-				id: parseInt(id),
-			},
-		})
-  console.log(material)
-	return { material }
-}) satisfies PageServerLoad;
+	console.log('LOADING ITEMS....')
+	// @ts-ignore
+	const { id } = params
+	const skip = parseInt(url.searchParams.get('skip') || '0')
+	const material = await db.material.findFirst({
+		where: {
+			id: parseInt(id),
+		},
+	})
+	const totalRows: PrismaPromise<number> = db.itensCompra.count({})
+
+	const itens = await db.itensCompra.findMany({
+		select: {
+			data: true,
+			anvisa: true,
+			fabricante: true,
+			fornecedor: true,
+			qtde: true,
+			valor_unitario: true,
+		},
+		orderBy: {
+			data: 'desc',
+		},
+		skip,
+		take: 20,
+	})
+	// console.log({ itens, totalRows, material })
+	return { itens, material, totalRows, take: 20 }
+}) satisfies PageServerLoad
--- src/routes/material/[id]/+page.svelte
+++ src/routes/material/[id]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import ItensTable from './itens-table.svelte'
 	import MaterialTable from '../material-table.svelte'
 	import * as Card from '$lib/components/ui/card'
 	import { Input } from '$lib/components/ui/input'
@@ -10,29 +11,43 @@
 	import { goto } from '$app/navigation'
 	import { debounce } from '$lib/utils'
 
-  export let data: PageData;
+	export let data: PageData
 
-  $: material = data?.material;
-  $: materials = [material]
+	$: material = data?.material
+	$: materials = [material]
+	$: rows = data?.itens
+	$: totalRows = data?.totalRows || 0
+  $: take = data?.take
+	$: console.log('ROWS ROWS:', { rows })
 
-
+  const handleChange = (event: CustomEvent) => {
+    let { skip } = event?.detail
+    const url = $page.url
+    url.searchParams.set('skip', skip)
+    goto(url, { replaceState: true, invalidateAll: true })
+  }
 </script>
 
 <Card.Root class="w-full mb-10">
-  <Card.Header>
-    <Card.Title tag="h4">Material selecionado:</Card.Title>
-  </Card.Header>
+	<Card.Header>
+		<Card.Title tag="h4">Material selecionado:</Card.Title>
+	</Card.Header>
 	<Card.Content>
-		<div class="flex flex-col max-h-[600px]">
-      <div class="flex-grow overflow-y-auto">
-        <MaterialTable
-          table={materials}
-          totalRows = 1
-        />
-      </div>
-      </div>
-      <Button class="w-full mt-10" href= {`/material/`}>
-        Alterar Material
-      </Button>
+		<div class="flex flex-col">
+			<div class="flex-grow overflow-y-auto">
+				<MaterialTable table={materials} totalRows="1" />
+			</div>
+		</div>
+		<Button class="w-full mt-10" href={`/material/`}>Alterar Material</Button>
 	</Card.Content>
 </Card.Root>
+
+{#if totalRows}
+	<div class="flex flex-col max-h-[1200px]">
+		<div class="flex-grow overflow-y-auto">
+			<ItensTable bind:table={rows} totalRows={totalRows} skip={0} take={take}
+      on:change={handleChange}
+			></ItensTable>
+		</div>
+	</div>
+{/if}
