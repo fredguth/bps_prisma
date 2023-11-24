f73d964e1557f11b45cfc44985e8f6c48138eced
Fred Guth <fredguth@fredguth.com>
2023-11-24 20:46:14 -0300

    installed turso db

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -5,16 +5,25 @@ import { createClient } from "@libsql/client";
 import { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } from "$env/static/private";
 
 // Create a new instance of the libSQL database client
-// const libsql = createClient({
-//   // @ts-ignore
-//   url: TURSO_DATABASE_URL?.trim(),
-//   authToken: TURSO_AUTH_TOKEN?.trim(),
-// });
+const libsql = createClient({
+	// @ts-ignore
+	url: TURSO_DATABASE_URL?.trim(),
+	authToken: TURSO_AUTH_TOKEN?.trim(),
+})
 
-// // Create a Prisma "adapter" for libSQL
-// const adapter = new PrismaLibSQL(libsql);
-// // Pass the adapter option to the Prisma Client instance
-// const prisma = new PrismaClient({ adapter });
+// Create a Prisma "adapter" for libSQL
+const adapter = new PrismaLibSQL(libsql)
+// Pass the adapter option to the Prisma Client instance
+const prisma = new PrismaClient({ adapter })
+
+if (dev) {
+	// @ts-ignore
+	global.__prisma = prisma
+}
+
+export default prisma
+
+// const prisma = global.__prisma || new PrismaClient();
 
 // if (dev) {
 //   // @ts-ignore
@@ -22,12 +31,3 @@ import { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } from "$env/static/private";
 // }
 
 // export default prisma;
-
-const prisma = global.__prisma || new PrismaClient();
-
-if (dev) {
-  // @ts-ignore
-  global.__prisma = prisma;
-}
-
-export default prisma;
