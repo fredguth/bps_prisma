d535db2edefbedc7504287a0a38f5afabac682e4
Fred Guth <fredguth@fredguth.com>
2023-11-24 16:17:51 -0300

    added logo

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,9 +1,13 @@
+<script>
+  import SVGImg from "./SVGImg.svelte";
+  import logo from '$lib/assets/bps.svg?raw';
+</script>
 <header
   class="text-zinc-800 text-sm relative px-10 shadow-lg shadow-slate-100 h-20 p-4 w-full bg-white"
 >
   <div class="flex container">
     <!-- <button style="text-wrap: nowrap;" class="text-blue-800 items-center cursor-pointer text-lg font-semibold justify-center relative text-center inline-flex w-8 h-8 rounded-full"><i class="font-black"></i></button> -->
-    <!-- <img src="/bps.png" alt="logo-bps" class="mr-4 h-12" /> -->
+    <SVGImg data={logo} size={40} class="mx-4"/>
 
     <div>
       <div class="text-[1.38rem] leading-7">Banco de Preços em Saúde</div>
