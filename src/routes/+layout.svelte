3b55240bb31c2b1f855a32b666db08ee290372fc
Fred Guth <fredguth@fredguth.com>
2023-12-05 12:43:34 -0300

    adds fade

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,29 @@
 <script lang="ts">
-  import "../app.postcss";
-  import { page } from "$app/stores";
-  import img from '$lib/assets/img-logo-MS.png';
-  import Header from '$lib/components/header.svelte';
-  import Footer from '$lib/components/footer.svelte';
-  import UserNav from "$lib/components/user-nav.svelte";
+	import '../app.postcss'
+	import { page } from '$app/stores'
+	import img from '$lib/assets/img-logo-MS.png'
+	import Header from '$lib/components/header.svelte'
+	import Footer from '$lib/components/footer.svelte'
+	import UserNav from '$lib/components/user-nav.svelte'
+  import { fade } from 'svelte/transition';
+	export let data
 
-  export let data;
-
-  $: classesActive = (href: string) =>
-   href === $page.url.pathname ? "!bg-primary-500" : "";
-  // $: console.log('layout', {data})
-  $: user = data?.user
+	$: classesActive = (href: string) =>
+		href === $page.url.pathname ? '!bg-primary-500' : ''
+	// $: console.log('layout', {data})
+	$: user = data?.user
 </script>
+
 <div class="flex flex-col min-h-screen">
-  <Header>
-    <UserNav slot="user-nav" {user} ></UserNav>
-    </Header>
-    <main class="flex-grow mx-auto p-8 container">
-    <slot />
-  </main>
-  <Footer />
+	<Header>
+		<UserNav slot="user-nav" {user}></UserNav>
+	</Header>
+	<main class="flex-grow mx-auto p-8 container">
+		{#key data.pathname}
+			<div in:fade={{ duration: 300, delay: 400 }} out:fade={{ duration: 300 }}>
+				<slot />
+			</div>
+		{/key}
+	</main>
+	<Footer />
 </div>
