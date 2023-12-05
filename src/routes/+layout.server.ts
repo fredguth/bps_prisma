16a2353b54320b67ab4030c3feede3b6a8ad08eb
Fred Guth <fredguth@fredguth.com>
2023-12-05 11:40:52 -0300

    fix bug login

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,6 +2,9 @@ import type { LayoutServerLoad } from './$types'
 // import { redirect } from '@sveltejs/kit'
 
 export const load: LayoutServerLoad = async ({ locals }) => {
-	const { user, session } = await locals.auth.validate()
+	if (locals.auth) {
+		const { user, session } = await locals.auth.validate()
 	return { user }
+	}
+
 }
